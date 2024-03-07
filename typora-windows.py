b250ad64c8d5d70119eedb7d077f636b67372b56
a1473838623 <71368946+a1473838623@users.noreply.github.com>
2024-03-07 22:04:21 +0800

    Update typora-windows.py

--- typora-windows.py
+++ typora-windows.py
@@ -1,29 +1,50 @@
+# python -m pip install requests
 import requests
 import sys
 import json
 import subprocess
 
+# WARNING: replace xxx with your joplin web clipper token
+token = "xxx"
+# WARNING: replace 41184 with your joplin web clipper port
+port = 41184
+# WARNING: replace python_exe_path with your python path, DO NOT DELELTE "r"
+python_exe_path = r'C:\Users\developer\AppData\Local\Programs\Python\Python312\python.exe'
+# WARNING: replace second_script_path with your second script path, DO NOT DELELTE "r"
+second_script_path = r'C:\Users\developer\Desktop\files\typora-windows-2.py'
+
+# collect args for typora-windows-2.py
 args = []
 for arg in sys.argv:
+    # the first arg is .py file, ignore it
     if(".py" in arg):
         continue
+    # the second arg is the current edting file passed by Typora
     if(".md" in arg):
         args.append(arg)
         continue
-    url = "http://127.0.0.1:41184/resources/?token=48c9b6a0a54d393a18b1d14d58f4c4432da5a1cd499118da598f47045766dd2a0b1b8923433c4fdcf84b5d6c61350591eba4eb4d9a325a35103a6cb1af59ffab"
+    # other arg is the local Typora-Image path passed by Typora
+    url = "http://127.0.0.1:"+port+"/resources/?token="+token
     payload = {'props': '{}'}
     files=[
       ('data',('typora.png',open(arg,'rb'),'image/png'))
     ]
     headers = {}
+    # upload your images to joplin resources using joplin web clipper API
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
-    newFilePath = "http://127.0.0.1:41184/resources/"+json.loads(response.text)["id"]+"/file?token=48c9b6a0a54d393a18b1d14d58f4c4432da5a1cd499118da598f47045766dd2a0b1b8923433c4fdcf84b5d6c61350591eba4eb4d9a325a35103a6cb1af59ffab"
+    newFilePath = "http://127.0.0.1:"+port+"/resources/"+json.loads(response.text)["id"]+"/file?token="+token
+    # print newFilePath, so Typora can replace the local-typora-image path with the joplin web clipper url.
+    # We can not directly using "resources/"+json.loads(response.text)["id"]+".png" since Typora think it is a illegal URL,
+    # and a warning will show up.
     print(newFilePath)
+
     args.append(newFilePath)
     args.append("resources/"+json.loads(response.text)["id"]+".png")
-second_script_path = r'C:\Users\developer\Desktop\files\typora-windows-2.py'
-subprocess.Popen([r'C:\Users\developer\AppData\Local\Programs\Python\Python312\python.exe', second_script_path] + args, creationflags=subprocess.DETACHED_PROCESS, shell=False)
+
+# first arg is .py file, second arg is .md file, then newFilePath and "resources/"+json.loads(response.text)["id"]+".png" appear in order
+# We use subprocess because Typora will wait for result of the first script and then replace the Typora_local_path with newFilePath.
+# If we don't use subprocess, our replacement will be ahead of Typora's, and covered by Typora, which makes newFilePath as the finally result link.
+# This is not we want, we want "resources/"+json.loads(response.text)["id"]+".png" as the result.
+subprocess.Popen([python_exe_path, second_script_path] + args, creationflags=subprocess.DETACHED_PROCESS, shell=False)
 
 sys.exit()
-
-
